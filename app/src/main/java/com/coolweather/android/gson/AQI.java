55eb4e0002498e3d9223e1b6a019eb964eab4e71
zxy <zxy@gmail.com>
2018-05-16 20:42:14 +0800

    完成天气显示界面

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by zxy on 2018/5/15.
+ */
+
+public class AQI {
+    public AQICity city;
+
+    public  class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
